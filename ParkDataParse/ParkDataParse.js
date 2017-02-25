f1882b556000ec30e43a11b8e4d224974b0d5f2e
Sally Calpo <spcalpo@gmail.com>
2017-02-24 20:15:33 -0800

    Added userAgent spoof; jsdom default user agent produced google search result similar to DeAnzaParkTestPage.html

--- ParkDataParse/ParkDataParse.js
+++ ParkDataParse/ParkDataParse.js
@@ -1,9 +1,11 @@
-var jsdom = require('jsdom').jsdom;
+var jsdom = require('jsdom');
 
 jsdom.env({
-  url: 'http://www.google.com/search?q=DE+ANZA+PARK+SAN+JOSE',
+  url: 'https://www.google.com/search?q=DE+ANZA+PARK+SAN+JOSE',
+  userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
   onload: (window) => {
-    var parkData = window.document.querySelector("div");
+    // var parkData = window.document.documentElement;
+    var parkData = window.document.querySelector(".navend");
     if(parkData !== null) {
       console.log(parkData.innerHTML);
     }
